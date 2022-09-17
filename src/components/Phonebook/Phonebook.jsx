7b92aae1a5395779c06d761e5b5bc65779c70d7d
JakubJarosz <jakubtm165@gmail.com>
2022-09-17 15:08:12 +0200

    ad

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -4,7 +4,7 @@ import FormAddContact from "../FormAddContact/FormAddContact";
 import SearchFilter from "../SearchFilter/SearchFilter";
 
 const Phonebook = () => {
-    const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('userInfo')) || []);
+    const [contacts, setContacts] = useState([]);
     const [filter, setFilter] = useState('');
 
 
